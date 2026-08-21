e6944c243fd3a9a5d0c9f4b04f5bb7a936d652ae
maxglomba <46114159+maxglomba@users.noreply.github.com>
2026-08-21 09:35:31 -0300

    fix: require preparer 0.0.92

--- install-onprem.sh
+++ install-onprem.sh
@@ -37,7 +37,7 @@ case $EDITION in
     ;;
   *) printf 'ERROR: Edicion de bootstrap invalida.\n' >&2; exit 1 ;;
 esac
-PREPARER_MIN_VERSION='0.0.91'
+PREPARER_MIN_VERSION='0.0.92'
 PREPARER_ACCESS_FILE="$INSTALL_ROOT/config/preparer-access.env"
 
 cleanup() {
